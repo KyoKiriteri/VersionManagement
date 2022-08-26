8bba36dfe0eb508c3b2634da3d2fe2ff44cad985
KyoKiriteri <sirppu04@gmail.com>
2022-08-26 13:24:04 +0300

    Working on a method to add new products, moving over to a branch

--- VersionManagement/VersionManagement/Program.cs
+++ VersionManagement/VersionManagement/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using InventoryManagement;
+
+//Variables
+var id = 0;
+var productname = "a";
+var productprice = 0;
+var productinventory = 0;
+
+
+//Starting point for the program
+Console.WriteLine("INVENTORY MANAGEMENT\n" +
+    "1 - Add a new product\n" +
+    "2 - Remove product\n" +
+    "3 - Print product inventory\n" +
+    "4 - Edit product name\n" +
+    "0 - Exit program");
+
+int initialInput = Convert.ToInt32(Console.ReadLine());
+
+//Switch, checking what to do with a specific input integer
+switch (initialInput)
+{
+    case 0:
+        Environment.Exit(0);
+        break;
+    case 1:
+        Console.WriteLine("Please input data in this order to add a new product\n" +
+            "Product name");
+        productname = Console.ReadLine();
+
+        Console.WriteLine("Product price");
+        productprice = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Product inventory");
+        productinventory = Convert.ToInt32(Console.ReadLine());
+        break;
+    case 2:
+        //RemoveProduct Method
+        break;
+    case 3:
+        //PrintProductInventory Method
+        break;
+    case 4:
+        //EditProfuctName Method
+        break;
+    default:
+        break;
+}
+
+//Method 1, AddProduct according to the user's inputs
+static bool AddNewProduct(int newId, string newProductName, int newProductPrice, int newProductInventory)
+{
+    using (VersionManagement versionManagement = new())
+    {
+        Product product = new()
+        {
+            Id = newId,
+            ProductName = newProductName,
+            ProductPrice = newProductPrice,
+            ProductInventory = newProductInventory
+        };
+
+        versionManagement.Products?.Add(product);
+
+        int affected = versionManagement.SaveChanges();
+        return (affected == 1);
+    }
+}
+
+
+
+
+if (AddNewProduct(id, productname, productprice, productinventory))
+{
+    Console.WriteLine("Product " + productname + 
+        " has been added with a price of " + productprice + 
+        " and inventory of " + productinventory + 
+        " residing in slot " + id + 
+        ".");
+}
+else
+{
+    Console.WriteLine("Incorrect data has been inputted.");
+}
--- VersionManagement/VersionManagement/VersionManagement.cs
+++ VersionManagement/VersionManagement/VersionManagement.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace VersionManagement
+namespace InventoryManagement
 {
     public class VersionManagement : DbContext
     {
